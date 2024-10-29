7977ec5cfdf07849e7b8f11be0e9efdc63fc5505
JacobCLarsen <jcla24@student.aau.dk>
2024-10-29 20:21:07 +0100

    made textbox for the user to type in. Also added a code font names "codefont"

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="mt-44 flex flex-col items-center justify-center">
+	<div class="font-codefont">
+		<p>int main &#123;</p>
+		<p class="my-4">const int hello = 67</p>
+		<p>&#125;</p>
+	</div>
+	<div class="mt-8">
+		<textarea
+			class="font-codefont mt-12 min-h-80 min-w-72 bg-slate-800 text-white"
+			name="text-input"
+			id="text-input"
+		></textarea>
+	</div>
+</div>
